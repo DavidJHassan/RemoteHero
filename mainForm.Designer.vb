6317f342f2932d060edb4791debe94c4dd6d4032
David Hassan <djhassan@lakeheadu.ca>
2015-06-20 22:18:35 -0400

    First code commit

---
+++ mainForm.Designer.vb
@@ -0,0 +1,70 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class mainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btn_Connect = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowDrop = True
+        Me.DataGridView1.AllowUserToOrderColumns = True
+        Me.DataGridView1.AllowUserToResizeRows = False
+        Me.DataGridView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(433, 352)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'btn_Connect
+        '
+        Me.btn_Connect.Location = New System.Drawing.Point(370, 383)
+        Me.btn_Connect.Name = "btn_Connect"
+        Me.btn_Connect.Size = New System.Drawing.Size(75, 31)
+        Me.btn_Connect.TabIndex = 1
+        Me.btn_Connect.Text = "Connect"
+        Me.btn_Connect.UseVisualStyleBackColor = True
+        '
+        'mainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(457, 426)
+        Me.Controls.Add(Me.btn_Connect)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "mainForm"
+        Me.Text = "RemoteHero"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents btn_Connect As System.Windows.Forms.Button
+
+End Class
---
+++ mainForm.vb
@@ -0,0 +1,117 @@
+﻿Imports System.DirectoryServices
+Imports System.Net
+Imports System.IO
+
+Public Class mainForm
+    Public hostNames As New List(Of String)
+    Public ipAddress As New List(Of String)
+    Public Sub New()
+
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+        readTable()
+        fillDataGridView()
+    End Sub
+
+    Public Sub readTable()
+        'Dim dataTable = File.Open("data_table.ini", FileMode.OpenOrCreate)
+        Dim parts As String()
+        Using reader As StreamReader = New StreamReader("data_table.ini")
+            Dim line As String
+            line = reader.ReadLine()
+            Do While (Not line Is Nothing And line <> "")
+                parts = line.Split(",")
+                If parts(0) <> "" And parts(1) <> "" Then
+                    hostNames.Add(parts(0))
+                    ipAddress.Add(parts(1))
+                End If
+                line = reader.ReadLine()
+            Loop
+        End Using
+    End Sub
+
+    Public Sub fillDataGridView()
+        Dim dataTable As New DataTable
+        dataTable.Columns.Add("Host Name")
+        dataTable.Columns.Add("TCP/IP")
+
+        Dim i As Integer = 0
+        While i < hostNames.Count
+            dataTable.Rows.Add(hostNames(i), ipAddress(i))
+            System.Math.Max(System.Threading.Interlocked.Increment(i), i - 1)
+        End While
+
+        DataGridView1.DataSource = dataTable
+        DataGridView1.Refresh()
+    End Sub
+
+    Public Sub executeCommandPrompt(ByVal ipaddress As String)
+        Dim p As Process = New Process()
+        'Process.Start("cmd", "/k mstsc /v: " & ipaddress & " /admin")
+        Process.Start("cmd", "/k echo " & ipaddress)
+    End Sub
+
+
+    'Dim de As New DirectoryEntry()
+    'de.Path = "LDAP://DC=BOWATER"
+
+
+    'Try
+    '    For Each d As DirectoryEntry In de.Children()
+    '        MsgBox(d.Name)
+    '    Next
+    'Catch ex As Exception
+    '    MsgBox(ex.Message)
+    'End Try
+
+
+    'Dim strHostName As New String("")
+    '' Getting Ip address of local machine...
+    '' First get the host name of local machine.
+    'strHostName = Dns.GetHostName()
+
+    '' Then using host name, get the IP address list..
+    'Dim ipEntry As IPHostEntry = Dns.GetHostByName(strHostName)
+    'Dim addr As IPAddress() = ipEntry.AddressList
+
+    'Dim i As Integer = 0
+    'Dim dataTable As New DataTable
+    'dataTable.Columns.Add("Host Name")
+    'dataTable.Columns.Add("TCP/IP")
+
+    'While i < addr.Length
+    '    dataTable.Rows.Add(Dns.GetHostByAddress(addr(i).ToString()).HostName, addr(i).ToString())
+    '    'hostName.Add()
+    '    'ip.Add()
+    '    System.Math.Max(System.Threading.Interlocked.Increment(i), i - 1)
+    'End While
+    'DataGridView1.DataSource = dataTable
+    'DataGridView1.Refresh()
+
+
+    Private Sub btn_Connect_Click(sender As Object, e As EventArgs) Handles btn_Connect.Click
+        'MsgBox(DataGridView1.CurrentCell.Value.ToString)
+
+        Dim FirstValue As Boolean = True
+        Dim cell As DataGridViewCell
+        Dim lastExecutedRow As Integer = -9999
+        For Each cell In DataGridView1.SelectedCells
+            Dim rowIndex As Integer = cell.OwningRow.Index
+            Dim colName As String = cell.OwningColumn.DataPropertyName
+
+            If lastExecutedRow <> rowIndex Then
+                If cell.Value <> "" Then
+                    executeCommandPrompt(cell.Value)
+                End If
+                lastExecutedRow = rowIndex
+            End If
+        Next
+    End Sub
+End Class
+
+
+
